c094272ac3785458ba65d5e85fec450ecfd3d3eb
Andro <https://github.com/AndroGamal>
2019-06-02 16:31:24 +0300

    record call

---
+++ app/src/main/java/com/example/andro/myapplication/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.andro.myapplication;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+    static Switch x;
+ListView l;
+    ArrayList vector;
+Button b1,b2;
+    File z;
+    String s,k="false";
+    MediaPlayer myMediaPlayer;
+
+    @Override
+    protected void onStart() {
+        vector.clear();
+        if(z.exists())
+        Collections.addAll(vector, z.list());
+        l.setAdapter(new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,vector));
+        super.onStart();
+    }
+    @Override
+    protected void onPostResume() {
+        vector.clear();
+        if(z.exists())
+        Collections.addAll(vector, z.list());
+        l.setAdapter(new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,vector));
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        x=findViewById(R.id.switch1);
+        l=findViewById(android.R.id.list);
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
+        vector=new ArrayList();
+       try {
+        k=new Scanner(new File("storage/sdcard0/.isStart.xml")).nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(k.equals("true")){
+            x.setChecked(true);
+        }
+        else { x.setChecked(false);}
+        z=new File("storage/sdcard0/AudioRecorder");
+         if(z.exists())
+        Collections.addAll(vector, z.list());
+        l.setAdapter(new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,vector));
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if(x.isChecked())
+                {
+                startService(new Intent(MainActivity.this,my.class));
+                }
+                else
+                {
+                    try {
+                        FileOutputStream writer=  new FileOutputStream(new File("storage/sdcard0/.isStart.xml"),false);
+                        writer.write("false".getBytes());
+                        writer.close();
+                    }
+                    catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                  stopService(new Intent(MainActivity.this, my.class));
+
+                }
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new File("storage/sdcard0/AudioRecorder/"+s).delete();
+                vector.clear();
+                Collections.addAll(vector, z.list());
+                l.setAdapter(new ArrayAdapter(MainActivity.this,android.R.layout.simple_expandable_list_item_1, vector));
+                Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+            }
+        });
+b2.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        myMediaPlayer.stop();
+        b2.setEnabled(false);
+    }
+});
+l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        s=(String) vector.get(position);
+        Toast.makeText(MainActivity.this, "select "+s, Toast.LENGTH_SHORT).show();
+        myMediaPlayer= MediaPlayer.create(MainActivity.this, Uri.parse("storage/sdcard0/AudioRecorder/"+s));
+        myMediaPlayer.start();
+        b2.setEnabled(true);
+    }
+});
+
+    }
+}
---
+++ app/src/main/java/com/example/andro/myapplication/my.java
@@ -0,0 +1,156 @@
+package com.example.andro.myapplication;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class my extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        runCallRecording();
+        return null;
+    }
+
+    boolean enter = true;
+    Handler handler = new Handler();
+    String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    MediaRecorder recorder = null;
+    static TelephonyManager telephonyManager;
+    int currentFormat = 0;
+    int output_formats[] = {MediaRecorder.OutputFormat.MPEG_4,
+            MediaRecorder.OutputFormat.THREE_GPP};
+    private String file_exts[] = {AUDIO_RECORDER_FILE_EXT_MP4,
+            AUDIO_RECORDER_FILE_EXT_3GP};
+
+    public void runCallRecording() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_OFFHOOK) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), "Start Call Recording", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    enter = false;
+                    recorder = new MediaRecorder();
+                    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    recorder.setOutputFormat(output_formats[currentFormat]);
+                    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    recorder.setOutputFile(getFilename());
+                    recorder.setOnErrorListener(errorListener);
+                    recorder.setOnInfoListener(infoListener);
+                    try {
+                        recorder.prepare();
+                        recorder.start();
+
+                    } catch (Exception e) {
+
+                    }
+                    while (!enter) {
+                        if (telephonyManager.getCallState() != TelephonyManager.CALL_STATE_OFFHOOK) {
+                            try {
+                                if (null != recorder) {
+                                    recorder.stop();
+                                    recorder.reset();
+                                    recorder.release();
+                                    recorder = null;
+                                }
+                            } catch (Exception stopException) {
+
+                            }
+                            enter = true;
+                        }
+                    }
+                }
+
+            }
+        }, 0, 1);
+
+      }
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            Toast.makeText(my.this,
+                    "Error: " + what + ", " + extra, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            Toast.makeText(my.this,
+                    "Warning: " + what + ", " + extra, Toast.LENGTH_SHORT)
+                    .show();
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            FileOutputStream writer = new FileOutputStream(new File("storage/sdcard0/.isStart.xml"), false);
+            writer.write("true".getBytes());
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        telephonyManager = (TelephonyManager) getSystemService(MainActivity.TELEPHONY_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        runCallRecording();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        try {
+            FileOutputStream writer = new FileOutputStream(new File("storage/sdcard0/.isStart.xml"), false);
+            writer.write("false".getBytes());
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(my.this, "Stop Call Recording", Toast.LENGTH_LONG).show();
+        super.onDestroy();
+    }
+}
+
